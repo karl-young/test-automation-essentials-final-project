fe6ffcbc32efbe7f44d0159721224472d90ef642
Karl Young <Karlyoung112@gmail.com>
2024-08-24 11:34:44 +1200

    Refactored my tests

--- Pages/ShoppingSite.ts
+++ Pages/ShoppingSite.ts
@@ -1,19 +1,22 @@
 import { Locator, Page } from '@playwright/test'
 const baseURL = 'https://react-shopping-cart-67954.firebaseapp.com/'
 export class ShoppingSite {
-  readonly page: page
+  readonly page: Page
   readonly addToCart: Locator
   readonly closeCheckout: Locator
   readonly cartAmount: Locator
   readonly removeItemButton: Locator
-  readonly sizeLocator: Locator 
-  
+  readonly sizeLocator: Locator
+  readonly openCheckout: Locator
+  readonly checkoutButton: Locator
+
   constructor(page: Page) {
-    let amount: number = 0
     this.page = page
     this.addToCart = page.getByRole('button', { name: 'Add to cart' })
     this.closeCheckout = page.getByRole('button', { name: 'X' })
     this.cartAmount = page.locator(`.sc-1h98xa9-9`)
+    this.openCheckout = page.locator('.sc-1h98xa9-2')
+    this.checkoutButton = page.getByRole('button', { name: 'Checkout' })
     this.removeItemButton = page.getByRole('button', {
       name: 'remove product from cart',
     })
@@ -21,17 +24,10 @@ export class ShoppingSite {
     page.goto(baseURL)
   }
 
-  async SelectSize(size: string) {
-    const sizeLocator = this.page.locator(`span.checkmark:has-text("${size}")`)
-    if ((await this.sizeLocator.count()) > 0) {
-      await this.sizeLocator.click()
-      await this.page.waitForLoadState('networkidle')
-    } else {
-      throw new Error(`Size" ${size}" Not Found`)
-    }
-  }
-  async clickAddToCart(index: number) {
-    await this.addToCart.nth(index).click()
+  async selectSize(size: string) {
+    const sizeLocator = this.page.locator(`//label[span[text()="${size}"]]`)
+    await sizeLocator.click()
+    await this.page.waitForLoadState('networkidle')
   }
 
   async clickCloseCheckout() {
@@ -39,20 +35,38 @@ export class ShoppingSite {
   }
 
   async getCartAmount() {
-    console.log(`Cart amount: ${await this.cartAmount.textContent()}`)
     return await this.cartAmount.textContent()
   }
 
+  async clickAddToCart(index: number) {
+    await this.addToCart.nth(index).click()
+  }
+
+  async countItemsInCart() {
+    return await this.removeItemButton.count()
+  }
+
+  async getItemsCount() {
+    return await this.addToCart.count()
+  }
+
   async addMultipleItems(count: number) {
-    const items = this.clickAddToCart(index)
-    console.log(`Amount of items: ${items}`)
-    for (let i = 0; i < Math.min(count, items); i++) {
+    const items = this.getItemsCount()
+    for (let i = 0; i < Math.min(count); i++) {
       await this.clickAddToCart(i)
       await this.clickCloseCheckout()
     }
   }
 
+  async clickOpenCheckout() {
+    await this.openCheckout.click()
+  }
+
   async ClickRemoveItem(index: number) {
     await this.removeItemButton.nth(index).click()
+  }
+
+  async clickCheckout() {
+    await this.checkoutButton.click()
   }
 }
--- tests/shoppingSite.spec.ts
+++ tests/shoppingSite.spec.ts
@@ -1,29 +1,22 @@
 import { test, expect } from '@playwright/test'
 import { ShoppingSite } from '../Pages/ShoppingSite'
 
+
 test.describe('Ecommerce Site', () => {
   
   test('checking large stock amount / another way', async ({ page }) => {
     const shoppingSite = new ShoppingSite(page)
-    await shoppingSite.SelectSize('L')
-    await page.waitForSelector('text=Add to cart', { state: 'visible' })
-    let itemList = page.getByRole('button', { name: 'Add to cart' })
-
-    const itemCount = await itemList.count()
-    console.log(`Amount of items: ${itemCount}`)
-    expect(itemCount).toBe(10)
+    await shoppingSite.selectSize('L')
+    
+    expect(await shoppingSite.getItemsCount()).toBe(10)
   })
 
   test('Add 6 items to cart', async ({ page }) => {
-    await page.goto(baseURL)
-    const itemList = page.getByRole('button', { name: 'Add to cart' })
-    for (let i = 0; i < 6; i++) {
-      await itemList.nth(i).click()
-      await page.getByRole('button', { name: 'X' }).click()
-    }
-    await page.getByRole('button', { name: '6' }).click()
-    const count = await page.locator('.sc-11uohgb-5').count()
-    expect(count).toBe(6)
+    const shoppingSite = new ShoppingSite(page)
+    await shoppingSite.addMultipleItems(6)
+    await shoppingSite.clickOpenCheckout()
+    
+    expect(await shoppingSite.countItemsInCart()).toBe(6)
   })
 
   test('Remove item from cart', async ({ page }) => {
@@ -35,47 +28,33 @@ test.describe('Ecommerce Site', () => {
   })
 
   test('Total price is correct', async ({ page }) => {
-    await page.goto(baseURL)
-    const items = page.getByRole('button', { name: 'Add to cart' })
-    for (let i = 0; i < 3; i++) {
-      await items.nth(i).click()
-      await page.getByRole('button', { name: 'X' }).click()
-    }
-    await page.getByRole('button', { name: '3' }).click()
-    const total = page.getByText('$ 50.05')
-    const totalText = await total.innerText()
-    expect(totalText).toBe('$ 50.05')
+    const shoppingSite = new ShoppingSite(page)
+    await shoppingSite.addMultipleItems(3)
+    await shoppingSite.clickOpenCheckout()
+    
+    expect(await shoppingSite.getCartAmount()).toBe('$ 50.05')
   })
 
   test('Checking math is correct when adding items', async ({ page }) => {
-    await page.goto(baseURL)
-    const items = page.getByRole('button', { name: 'Add to cart' })
-    for (let i = 0; i < 3; i++) {
-      await items.nth(i).click()
-      await page.getByRole('button', { name: 'X' }).click()
-    }
-    await page.getByRole('button', { name: '3' }).click()
-    const totalText = await page.getByText('$ 50.05').innerText()
-    expect(totalText).toBe('$ 50.05')
-    await page
-      .getByRole('button', { name: 'remove product from cart' })
-      .first()
-      .click()
-    const subtotalText = await page.getByText('$ 39.15').innerText()
-    expect(subtotalText).toBe('$ 39.15')
+    const shoppingSite = new ShoppingSite(page)
+    await shoppingSite.addMultipleItems(3)
+    await shoppingSite.clickOpenCheckout()
+    
+    expect(await shoppingSite.getCartAmount()).toBe('$ 50.05')
+    await shoppingSite.removeItemButton.first().click()
+    
+    expect(await shoppingSite.getCartAmount()).toBe('$ 39.15')
   })
 
   test('Complete order', async ({ page }) => {
-    await page.goto(baseURL)
-    const items = page.getByRole('button', { name: 'Add to cart' })
-    for (let i = 0; i < 3; i++) {
-      await items.nth(i).click()
-      await page.getByRole('button', { name: 'X' }).click()
-    }
+    const shoppingSite = new ShoppingSite(page)
+    await shoppingSite.addMultipleItems(3)
+    await shoppingSite.clickOpenCheckout()
+    await shoppingSite.clickCheckout()
+    
 
-    await page.getByRole('button', { name: '3' }).click()
-    await page.getByRole('button', { name: 'Checkout' }).click()
-
+    
+    // I still cant get the popup to work for some reason
     const checkoutText = await page
       .getByText('Checkout - Subtotal: $ 50.05')
       .innerText()
