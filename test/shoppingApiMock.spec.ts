cca7c7a88b01b2f58bbdb15e34329b4f32050ff5
Karl Young <Karlyoung112@gmail.com>
2024-09-10 15:09:16 +1200

    fixed mock test

--- test/shoppingApiMock.spec.ts
+++ test/shoppingApiMock.spec.ts
@@ -21,7 +21,7 @@ test.describe('Shopping API Mock', () => {
     });
   });
   
-  test.fixme("Gets real response from API and adds some mock data.", async ({ page }) => {
+  test("Gets real response from API and adds some mock data.", async ({ page }) => {
 
     await page.route('**/products.json', async route => {
       // fetch the original response
