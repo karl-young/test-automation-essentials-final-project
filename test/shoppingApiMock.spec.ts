c2de05257a777ec8142d5e6319eacf03c85cc280
Karl Young <Karlyoung112@gmail.com>
2024-09-11 20:41:49 +1200

    Added steps for mock test

--- test/shoppingApiMock.spec.ts
+++ test/shoppingApiMock.spec.ts
@@ -8,44 +8,48 @@ test.describe('Shopping API Mock', () => {
       await page.route('**/products.json', async (route) => {
         route.fulfill({
           body: JSON.stringify(mockProductsJson),
-        });
-      });
-    });
-    
-     
-    await test.step('Visit the site', async () => {
-      await page.goto(`${baseURL}/products.json`);
-    });
-    
-    await test.step('Verify products are displayed', async () => {
-      await expect(page.getByText('White T-shirt With Cactus Print')).toBeVisible();
-    });
-  });
-  
-  test("Gets real response from API and adds some mock data.", async ({ page }) => {
-
-    await page.route('**/products.json', async route => {
-      // fetch the original response
-      const response = await route.fetch()
-      const json = await response.json();
-      json.products.push({
-        ...mockProductsJson
+        })
       })
-      console.log('Mocked JSON:', json)
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify(json), 
-      });
+    })
 
-      
-    });
-    
-    await page.goto(baseURL);
+    await test.step('Visit the site', async () => {
+      await page.goto(`${baseURL}/products.json`)
+    })
 
-    await expect(page.getByText('White T-shirt With Cactus Print')).toBeVisible();
-
-
+    await test.step('Verify products are displayed', async () => {
+      await expect(
+        page.getByText('White T-shirt With Cactus Print')
+      ).toBeVisible()
+    })
   })
-  
+
+  test('Gets real response from API and adds some mock data.', async ({
+    page,
+  }) => {
+    await test.step('Intercept the API request and mock the response', async () => {
+      await page.route('**/products.json', async (route) => {
+        // fetch the original response
+        const response = await route.fetch()
+        const json = await response.json()
+        json.products.push({
+          ...mockProductsJson,
+        })
+        await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: JSON.stringify(json),
+        })
+      })
+    })
+
+    await test.step('Visit the site', async () => {
+      await page.goto(baseURL)
+    })
+
+    await test.step('Verify products are displayed', async () => {
+      await expect(
+        page.getByText('White T-shirt With Cactus Print')
+      ).toBeVisible()
+    })
+  })
 })
