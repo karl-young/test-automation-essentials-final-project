38cbb9011b225be97dd6f0523507404a9ffa1c01
Karl Young <Karlyoung112@gmail.com>
2024-09-13 17:46:33 +1200

    got first test passing

--- Pages/TMMainPage.ts
+++ Pages/TMMainPage.ts
@@ -1,30 +1,43 @@
 import { Locator, Page } from '@playwright/test'
+import { parseNumberWithCommas } from '../utils/utility'
 
 const TMbaseURL = 'https://www.trademe.co.nz/a/motors'
 
 export class TMMainPage {
   readonly page: Page
-  readonly makeDropdown: (make: string) => Locator
+  readonly makeDropdown: Locator
   readonly viewListings: Locator
+  readonly showingResults: Locator
 
   constructor(page: Page) {
     this.page = page
-    this.makeDropdown = (make: string) => this.page.locator(`select[name="make"] > option[value="${make}"]`)
+    this.makeDropdown = this.page.getByLabel('Make (optional) ')
     this.viewListings = page.getByRole('button', { name: 'View listings' })
+    this.showingResults = page.getByRole('heading', {
+      name: /Showing \d{1,3}(,\d{3})* results/,
+    })
   }
-  
+
   async navigateToSite() {
-    await this.page.goto(TMbaseURL)
-    await this.page.waitForLoadState('networkidle')
+    await this.page.goto(TMbaseURL, { waitUntil: 'load' })
   }
 
   async selectMake(make: string) {
-    await this.makeDropdown(make).click()
+    await this.makeDropdown.selectOption({ label: make })
   }
 
   async clickViewListings() {
     await this.viewListings.click()
   }
 
-
+  async getResultsCount() {
+    await this.showingResults.waitFor({ state: 'visible' })
+    let resultsText = await this.showingResults.textContent()
+    let resultsMatch = resultsText?.match(/[\d,]+/)
+    if (resultsMatch) {
+      let parsedNumber = parseNumberWithCommas(resultsMatch[0])
+      return parsedNumber
+    }
+    return 0
+  }
 }
--- test/TradeMe.spec.ts
+++ test/TradeMe.spec.ts
@@ -6,12 +6,29 @@ test.describe('Tests Trademe car makes', () => {
 
   test.beforeEach(async ({ page }) => {
     tmMainPage = new TMMainPage(page)
-    await tmMainPage.navigateToSite() 
+    await tmMainPage.navigateToSite()
   })
 
   test('verify the car makes are displayed correctly', async ({ page }) => {
-    await tmMainPage.selectMake('Ferrari')
-    await tmMainPage.clickViewListings()
+    let expectedMakes = {
+      BMW: { min: 3139, max: 3339 }, // 3239 ± 100
+      Ferrari: { min: 23, max: 63 }, // 43 ± 20
+      Mazda: { min: 6171, max: 6371 }, // 6271 ± 100
+      Honda: { min: 3436, max: 3536 }, // 3486 ± 50
+    }
+
+    for (const [make, expectedRange] of Object.entries(expectedMakes)) {
+      await test.step(`Given the customer has selected the make "${make}"`, async () => {
+        await tmMainPage.selectMake(make)
+      })
+      await tmMainPage.clickViewListings()
+      
+      await test.step(`Then the stock level for the "${make}" should be between ${expectedRange.min} and ${expectedRange.max}.`, async () => {
+        const actualStock = await tmMainPage.getResultsCount()
+        expect(actualStock).toBeGreaterThanOrEqual(expectedRange.min)
+        expect(actualStock).toBeLessThanOrEqual(expectedRange.max)
+      })
+      await tmMainPage.navigateToSite()
+    }
   })
 })
- 
