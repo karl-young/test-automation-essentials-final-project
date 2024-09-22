fd4b2d74c696bfdc9c5bccb5e48c093d5286f418
Karl Young <Karlyoung112@gmail.com>
2024-09-22 12:37:59 +1200

    Took await timeouts and changed folder for yml report

--- Pages/tMMainPage.ts
+++ Pages/tMMainPage.ts
@@ -50,12 +50,8 @@ export class TMMainPage {
     await filterLocator.waitFor({ state: 'visible' })
     await filterLocator.click()
 
-    await this.page.waitForTimeout(600)
-
     await this.viewResults.waitFor({ state: 'visible' })
     await this.viewResults.click()
-
-    await this.page.waitForTimeout(600)
   }
 
   async clickViewListings() {
